1b99d685faae20ced5a77972db63de16f77cd749
duyle <duyleekun@gmail.com>
2023-04-20 18:05:07 +0700

    Disable named range

--- src/gas/lib/sheet_util.ts
+++ src/gas/lib/sheet_util.ts
@@ -1,8 +1,20 @@
 export function writeToSheet(sheetName, data: any[]) {
     const headersSet = new Set<string>()
 
+    let spreadSheet = SpreadsheetApp.getActiveSpreadsheet()
+    let sheet = spreadSheet.getSheetByName(sheetName);
+    if (!sheet) {
+        sheet = spreadSheet.insertSheet(sheetName);
+    }
+    sheet.clearContents();
+    sheet.clear()
+    console.log('data.length', data.length)
+    if (data.length == 0) {
+        return
+    }
+
     //Sampling header from top 10
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < data.length; i++) {
         // console.log(Object.keys(data[0]))
         // console.log(data[0])
         if (data[i])
@@ -15,21 +27,24 @@ export function writeToSheet(sheetName, data: any[]) {
         if (datum)
             sheetData.push(headers.map((it) => datum[it]));
     }
-    let spreadSheet = SpreadsheetApp.getActiveSpreadsheet()
-    let sheet = spreadSheet.getSheetByName(sheetName);
-    if (!sheet) {
-        sheet = spreadSheet.insertSheet(sheetName);
-    }
-    sheet.clearContents();
 
     const range = sheet.getRange(1, 1, sheetData.length, sheetData[0].length)
+    console.log('before range.getFilter()?.remove()')
     range.getFilter()?.remove()
+    console.log('before range.setValues(sheetData)')
     range.setValues(sheetData);
+    console.log('before range.createFilter()')
     range.createFilter();
-    for (const col in headers) {
-        console.log('named range', `${sheetName}.${headers[col]}`)
-        if (headers[col].split('.').length <= 1) {
-            spreadSheet.setNamedRange(`${sheetName}.${headers[col]}`, sheet.getRange(2, parseInt(col) + 1, sheetData.length - 1, 1))
-        }
-    }
+    console.log('end range.createFilter()')
+    // for (const col in headers) {
+    //     console.log('${sheetName}.${headers[col]}', `${sheetName}.${headers[col]}`)
+    //     console.log('headers[col].split(\'.\').length <= 1', headers[col].split('.').length <= 1)
+    //     if (headers[col].split('.').length <= 1) {
+    //         try {
+    //             // spreadSheet.setNamedRange(`${sheetName}.${headers[col]}`, sheet.getRange(2, parseInt(col) + 1, sheetData.length - 1, 1))
+    //         } catch (e) {
+    //             console.error(e)
+    //         }
+    //     }
+    // }
 }
